42b774e401e2e56f6c15c26e47cda562dd8ddb86
nicknt <nikolasderm@gmail.com>
2024-10-20 23:58:08 +0300

    add the super solution

--- lab02.s
+++ lab02.s
@@ -1,18 +1,36 @@
 
 .data
 
-array: .word 1, 0, 1, 12, 0, 1, 4
+array: .word 10, 2, -5
 
 .text
-
-    la a0, array
-    li a1, 7    # unsigned
-    li a2, 1
+    la a0, array # address of array
+    li a1, 3     # length of array
+    li a2, 10     # value to search for
 prog:
-#-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
-#-----------------------------
+    add t0, zero, a1        # Load the length of the array to the t0
+                            # t0 will be the index
+    add s0, zero, zero      # t1 will be the final result. 
+                            # if not found the t1 will be 0
+    beq a1, zero, zeros        # if the length is 1, we don't need to search
+    addi t0, t0, -1
+    
+loop:
+    blt t0, zero, zeros
+    add t2, t0, zero        # Load the t2 with the index
+    slli t2, t2, 2          # shift left by 2 == multiply by 4
+    add t2, t2, a0          # add the address of the array to the t2
+                            # finally t2 is the address of the element we want
+    lw s0, 0(t2)            # loading the t3 with the actuall array value
+    addi t0, t0, -1         # decrease the counter t0
+    bne s0, a2, loop        # end the loop if the value is found
+                            # else if we have t3 == a2 we found it and continue
+    add s0, zero, t2
+    j done
+
+zeros:
+    add s0, zero, zero
+
 done:
     addi a7, zero, 10 
     ecall
